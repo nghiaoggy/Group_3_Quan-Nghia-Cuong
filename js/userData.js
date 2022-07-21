583167391effe40a831ba7deefc59d77e3f587c8
Quân Nguyễn Anh <quan.nguyenanh304@gmail.com>
2022-07-21 23:22:55 +0700

    update

--- js/userData.js
+++ js/userData.js
@@ -1,19 +1,5 @@
-let Quan = {
-     name: "Quan",
-     username: "AnhQuan123",
-     password: "quan1234"
-};
-
-let Nghia = {
-    name: "Nghia",
-    username: "Nghia123",
-    password: "nghia1234"
-};
-
-let Cuong = {
-    name: "Cuong",
-    username: "Cuong123",
-    password: "cuong1234"
-};
-
-const user = { Quan, Nghia, Cuong};
+ const people = [
+    { name: 'Repairing'},
+    { name: 'Cleaning'},
+    { name: 'Assembling'}
+]
