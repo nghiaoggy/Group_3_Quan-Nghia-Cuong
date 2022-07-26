f888799a96d95ee9fa40efd692f3d637f4145e94
nghiaoggy <57754551+nghiaoggy@users.noreply.github.com>
2022-07-26 10:39:01 +0700

    Update

--- js/Register.js
+++ js/Register.js
@@ -1,3 +1,20 @@
-function ktra(){
-    let email = String(document.getElementById("disableEmail"))
+function open1(){
+    var x = document.getElementById("Menu")
+    x.style.display = 'none';
+    x = document.getElementById("for_partner")
+    x.style.display = 'block';
+}
+function open2(){
+    var x = document.getElementById("Menu")
+    x.style.display = 'none';
+
+    x = document.getElementById("for_user")
+    x.style.display = 'block';
+}
+function userChange() {
+    var x = document.getElementById("Selection").value;
+    if (x==="other") {
+        document.getElementById("demo").innerHTML = `<input class="form-control ml-3 mt-3" id="Certification" type="text" placeholder="Your Certification">`;
+    }
+    else document.getElementById("demo").innerHTML = ``;
 }
